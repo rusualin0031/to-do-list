71b13e9180c50551306f2b8f4db9447790008968
rusualin <rusualin0031@gmail.com>
2024-03-23 08:13:21 +0100

    styling button edit

--- src/ToDoList.jsx
+++ src/ToDoList.jsx
@@ -1,12 +1,10 @@
-import {useEffect, useState} from "react";
-
-
+import React, { useEffect, useState } from "react";
 
 function ToDoList() {
     const [input, setInput] = useState("");
     const [tasks, setTasks] = useState([]);
-    const [editingTask, setEditingTask] = useState();
-    // const [newTask, setNewTask] = useState("");
+    const [editingTask, setEditingTask] = useState(null);
+    const [editedTaskLabel, setEditedTaskLabel] = useState("");
 
     const handleInputChange = (event) => {
         setInput(event.target.value);
@@ -19,54 +17,73 @@ function ToDoList() {
     }
 
     const handleDeleteTask = (task) => {
-        if (window.confirm("Do you really want to leave?")) {
+        if (window.confirm("Do you really want to delete?")) {
             const newTasks = tasks.filter(
                 (t) => t !== task
-            ) 
+            );
             setTasks(newTasks);
         }
-     }
-        
-
-    const handleSubmit = () => {
-      if(input.trim() === '') return;
-
-      const newTask = {
-        label: input.trim(),
-        isChecked: false,
-      };
-      setTasks([
-        ...tasks,
-        newTask
-      ]);
-      setInput("");
     }
 
-    useEffect( () => {
-      const tasks = localStorage.getItem('tasks');
-      if (tasks) {
-        setTasks(JSON.parse(tasks));
-      }
+    const handleSubmit = () => {
+        if (input.trim() === '') return;
+
+        const newTask = {
+            label: input.trim(),
+            isChecked: false,
+        };
+        setTasks([
+            ...tasks,
+            newTask
+        ]);
+        setInput("");
+    }
+
+    useEffect(() => {
+        const tasks = localStorage.getItem('tasks');
+        if (tasks) {
+            setTasks(JSON.parse(tasks));
+        }
     }, []);
 
     useEffect(() => {
-      localStorage.setItem('tasks', JSON.stringify(tasks));
+        localStorage.setItem('tasks', JSON.stringify(tasks));
     }, [tasks]);
 
     const handleChangeIsChecked = (task, isChecked) => {
-      const newTasks = tasks.map((t) => {
-        if (t === task) {
-          return {
-            label: task.label,
-            isChecked: isChecked,
-          };
-        }
-        return t;
-      });
-      setTasks(newTasks);
+        const newTasks = tasks.map((t) => {
+            if (t === task) {
+                return {
+                    label: task.label,
+                    isChecked: isChecked,
+                };
+            }
+            return t;
+        });
+        setTasks(newTasks);
     }
 
-    return(
+    const handleEditButtonClick = (task) => {
+        setEditingTask(task);
+        setEditedTaskLabel(task.label);
+    }
+
+    const handleSaveEdit = (task, editedName) => {
+        const newTasks = tasks.map((t) => {
+            if (t === task) {
+                return {
+                    ...t,
+                    label: editedName,
+                };
+            }
+            return t;
+        });
+        setTasks(newTasks);
+        setEditingTask(null);
+        setEditedTaskLabel("");
+    }
+
+    return (
         <>
             <div className="to-do-list">
                 <h1>To-Do-List</h1>
@@ -86,13 +103,29 @@ function ToDoList() {
             <div className="toDoListItems">
                 {tasks.map((task, index) => (
                     <div className="toDoListItem" key={index}>
-                        <input type="checkbox" checked={task.isChecked} onChange={(ev) => {
-                        handleChangeIsChecked(task, ev.target.checked);
-                        }} />
-                        <span className="itemLabel">{task.label}</span>
-                        <button type="button" onClick={ () => {
-                            handleDeleteTask(task)
-                        } }>Delete</button>
+                        <input type="checkbox" checked={task.isChecked} onChange={(ev) => handleChangeIsChecked(task, ev.target.checked)} />
+                        {editingTask === task ? (
+                            <>
+                                <input
+                                    type="text"
+                                    value={editedTaskLabel}
+                                    onChange={(e) => setEditedTaskLabel(e.target.value)}
+                                    onBlur={() => handleSaveEdit(task, editedTaskLabel)}
+                                    onKeyUp={(e) => {
+                                        if (e.key === 'Enter') {
+                                            handleSaveEdit(task, editedTaskLabel);
+                                        }
+                                    }}
+                                />
+                                <button onClick={() => handleSaveEdit(task, editedTaskLabel)}>Save</button>
+                            </>
+                        ) : (
+                            <>
+                                <span className="itemLabel">{task.label}</span>
+                                <button className="button-edit" onClick={() => handleEditButtonClick(task)}>Edit</button>
+                                <button className="button-delete" onClick={() => handleDeleteTask(task)}>Delete</button>
+                            </>
+                        )}
                     </div>
                 ))}
             </div>
@@ -100,4 +133,4 @@ function ToDoList() {
     );
 }
 
-export default ToDoList
+export default ToDoList;
