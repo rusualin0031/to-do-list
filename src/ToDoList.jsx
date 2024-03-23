679ad25cc224c065bc02344c9bd0c40e9ab8bd31
rusualin <rusualin0031@gmail.com>
2024-03-23 08:48:12 +0100

    create const reorderList

--- src/ToDoList.jsx
+++ src/ToDoList.jsx
@@ -83,6 +83,13 @@ function ToDoList() {
         setEditedTaskLabel("");
     }
 
+    const reorderList = (list, startIndex, endIndex) => {
+        const result = Array.from(list);
+        const [removed] = result.splice(startIndex, 1);
+        result.splice(endIndex, 0, removed);
+        return result;
+    };
+
     return (
         <>
             <div className="container">
