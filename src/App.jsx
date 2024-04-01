f34165a594677ea3aa33287ae40fb50f69beaca4
rusualin <rusualin0031@gmail.com>
2024-04-01 22:40:48 +0200

    modified file App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,18 @@
-import ToDoList from './components/ToDoList'
-import './App.scss'
+import React from 'react';
+import { Provider } from 'react-redux';
+import store from './store'; 
+import UsernameForm from './components/UsernameForm'; 
+import ToDoList from './components/ToDoList'; 
+import './styling/App.scss';
 
 function App() {
   return (
-    <>
+    <Provider store={store}>
+      <h1>Project</h1>
+      <UsernameForm />
       <ToDoList />
-    </>
-  )
+  </Provider>
+);
 }
 
 export default App
