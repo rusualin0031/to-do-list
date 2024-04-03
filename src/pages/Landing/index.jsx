f9fbd9d83c5dfff556d2f18387b74dd3269a1baa
rusualin <rusualin0031@gmail.com>
2024-04-03 20:40:36 +0200

    create function enter keyboard

--- src/pages/Landing/index.jsx
+++ src/pages/Landing/index.jsx
@@ -4,35 +4,42 @@ import './style.scss';
 import {setUsername} from "../../store/slices/authSlice.js";
 
 function Landing () {
-  const [inputUsername, setInputUsername] = useState( '' );
-  const dispatch = useDispatch();
+    const [inputUsername, setInputUsername] = useState( '' );
+    const dispatch = useDispatch();
 
-  const handleInputChange = ( event ) => {
-    setInputUsername( event.target.value );
-  };
+    const handleInputChange = ( event ) => {
+        setInputUsername( event.target.value );
+    };
 
-  const handleSetUsername = () => {
-    dispatch( setUsername( inputUsername ) );
-    setInputUsername( '' );
-  };
+    const handleSetUsername = () => {
+        dispatch(setUsername( inputUsername ) );
+        setInputUsername( '');
+    };
 
-  return (
-    <div className="login-card-container">
-      <div className="login-card">
-        <h2>User Name</h2>
-        <div className="login-form">
-          <input
-            type="text"
-            value={inputUsername}
-            onChange={handleInputChange}
-            placeholder="Enter your username"
-            className="input-username"
-          />
-          <button onClick={handleSetUsername} className="button-login">Login</button>
+    const handleKeyUp = (event) => {
+        if (event.key === "Enter") {
+            handleSetUsername();
+        }
+    };
+
+    return (
+        <div className="login-card-container">
+            <div className="login-card">
+                <h2>User Name</h2>
+                <div className="login-form">
+                    <input
+                        type="text"
+                        value={inputUsername}
+                        onChange={handleInputChange}
+                        onKeyUp={handleKeyUp}
+                        placeholder="Enter your username"
+                        className="input-username"
+                    />
+                    <button onClick={handleSetUsername} className="button-login">Login</button>
+                </div>
+            </div>
         </div>
-      </div>
-    </div>
-  );
+    );
 }
 
 export default Landing;
